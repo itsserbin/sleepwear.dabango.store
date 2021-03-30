ba37b86d0cbee99f9f2cbce2433ccb0e9c862164
AnyMac Store <anymacstore@MacBook-Pro-AnyMac.local>
2021-03-30 18:10:51 +0300

    ttt

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'h1',
+        'content',
+        'characteristics',
+        'cost',
+        'sale_cost',
+        'preview',
+        'images',
+    ];
+
+    public function ProductsPhoto()
+    {
+        return $this->hasMany('App\ProductsPhoto', 'product_id', 'id');
+    }
 }
--- app/Models/ProductsPhoto.php
+++ app/Models/ProductsPhoto.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ProductsPhoto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'images',
+        'product_id',
+    ];
+
+    public function Products()
+    {
+        return $this->belongsTo(Products::class, 'product_id', 'id');
+    }
 }
