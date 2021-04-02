be534ded5f93f467ede45be539b7149df6c8b567
anymacstore <9911>
2021-04-02 23:20:06 +0300

    fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,14 +2,14 @@
     <div class="row">
         <div id="contacts" class="footer__first-side">
             <div class="footer__title">Контакты</div>
-            <div class="footer__location location">
-                <a href="https://goo.gl/maps/t2PTuXMWEhXQq6D47" target="_blank">
-                    вулиця Полковника Кедровського, 1, Херсон, Херсонська область
-                </a>
-            </div>
+{{--            <div class="footer__location location">--}}
+{{--                <a href="https://goo.gl/maps/t2PTuXMWEhXQq6D47" target="_blank">--}}
+{{--                    вулиця Полковника Кедровського, 1, Херсон, Херсонська область--}}
+{{--                </a>--}}
+{{--            </div>--}}
             <address class="footer__address address">
-                <a href="tel:380683916211" class="phonecall">+38 (068) 391-62-11</a>
-                <a href="mailto:lucci@gmail.com" class="email">lucci@gmail.com</a>
+                <a href="tel:+380636020071" class="phonecall">+38 (063) 602-00-71</a>
+                <a href="mailto:youbrandtop@gmail.com" class="email">youbrandtop@gmail.com</a>
             </address>
         </div>
         <div class="footer__second-side">
