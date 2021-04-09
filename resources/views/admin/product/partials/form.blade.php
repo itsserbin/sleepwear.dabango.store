a607673af84c4c4022e9040a6c001332d4d4572c
anymacstore <9911>
2021-04-09 03:41:03 +0300

    add colors

--- resources/views/admin/product/partials/form.blade.php
+++ resources/views/admin/product/partials/form.blade.php
@@ -44,7 +44,7 @@
                     <input type="checkbox" class="btn-check" name="color[]" id="{{$item->color}}" autocomplete="off"
                            value="{{$item->color}}"
                            @if($item->color) checked @endif>
-                    <label class="btn btn-outline-primary" for="{{$item->color}}">{!! $item->color !!}</label>
+                    <label class="btn btn-outline-primary my-1" for="{{$item->color}}">{!! $item->color !!}</label>
                 @endforeach
             </div>
 
@@ -53,39 +53,47 @@
             <h3>Все цвета:</h3>
             <div class="col-12">
                 <input type="checkbox" class="btn-check" name="color[]" id="black" autocomplete="off" value="Черный">
-                <label class="btn btn-outline-primary" for="black">Черный</label>
+                <label class="btn btn-outline-primary my-1" for="black">Черный</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="white" autocomplete="off" value="Белый">
-                <label class="btn btn-outline-primary" for="white">Белый</label>
+                <label class="btn btn-outline-primary my-1" for="white">Белый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="gray" autocomplete="off" value="Серый">
-                <label class="btn btn-outline-primary" for="gray">Серый</label>
+                <label class="btn btn-outline-primary my-1" for="gray">Серый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="red" autocomplete="off" value="Красный">
-                <label class="btn btn-outline-primary" for="red">Красный</label>
+                <label class="btn btn-outline-primary my-1" for="red">Красный</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="pink" autocomplete="off" value="Розовый">
-                <label class="btn btn-outline-primary" for="pink">Розовый</label>
+                <label class="btn btn-outline-primary my-1" for="pink">Розовый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="powdery" autocomplete="off" value="Пудровый">
-                <label class="btn btn-outline-primary" for="powdery">Пудровый</label>
+                <label class="btn btn-outline-primary my-1" for="powdery">Пудровый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="burgundy" autocomplete="off"
                        value="Бордовый">
-                <label class="btn btn-outline-primary" for="burgundy">Бордовый</label>
+                <label class="btn btn-outline-primary my-1" for="burgundy">Бордовый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="blue" autocomplete="off" value="Синий">
-                <label class="btn btn-outline-primary" for="blue">Синий</label>
+                <label class="btn btn-outline-primary my-1" for="blue">Синий</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="green" autocomplete="off" value="Зеленый">
-                <label class="btn btn-outline-primary" for="green">Зеленый</label>
+                <label class="btn btn-outline-primary my-1" for="green">Зеленый</label>
 
                 <input type="checkbox" class="btn-check" name="color[]" id="turquoise" autocomplete="off"
                        value="Бирюзовый">
-                <label class="btn btn-outline-primary" for="turquoise">Бирюзовый</label>
+                <label class="btn btn-outline-primary my-1" for="turquoise">Бирюзовый</label>
 
-                <input type="checkbox" class="btn-check" name="color[]" id="yellow" autocomplete="off" value="Желтый">
-                <label class="btn btn-outline-primary" for="yellow">Желтый</label>
+                <input type="checkbox" class="btn-check" name="color[]" id="turquoise" autocomplete="off"
+                       value="Мятный">
+                <label class="btn btn-outline-primary my-1" for="turquoise">Мятный</label>
+
+                <input type="checkbox" class="btn-check" name="color[]" id="mint" autocomplete="off" value="Желтый">
+                <label class="btn btn-outline-primary my-1" for="mint">Желтый</label>
+
+                <input type="checkbox" class="btn-check" name="color[]" id="citric" autocomplete="off" value="Лимонный">
+                <label class="btn btn-outline-primary my-1" for="citric">Лимонный</label>
+
             </div>
         </div>
 
