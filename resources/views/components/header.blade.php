34108b804dc4194b1bfded7ac240918a75397be0
anymacstore <9911>
2021-04-11 19:33:59 +0300

    link logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,9 @@
 <div class="content">
     <div class="row">
         <div class="header__logo logo">
-            <a href="{{route('home')}}"></a>
-            <img src="{{asset('storage/img/content/logo.png')}}" alt="">
+            <a href="{{route('home')}}">
+                <img src="{{asset('storage/img/content/logo.png')}}" alt="">
+            </a>
         </div>
         <div class="header-burger">
             <span class="icon-menu11"></span>
