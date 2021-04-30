639f44e0972f7cd9942d55ae808ff4847b3f46b8
anymacstore <9911>
2021-05-01 02:51:57 +0300

    set crone daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,12 +26,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
             $date = Carbon::yesterday();
 
@@ -44,7 +43,7 @@ class Kernel extends ConsoleKernel
             $profit->marginality = $ProfitProfit - $ProfitCost;
             $profit->turnover = $ProfitProfit + $ProfitCost;
             $profit->save();
-        })->everyMinute();
+        })->daily();
     }
 
     /**
@@ -54,7 +53,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
