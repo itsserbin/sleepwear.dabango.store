275afce06822ad9e9645c2fdb8bc90918ac83565
anymacstore <9911>
2021-05-04 00:23:26 +0300

    csrf review

--- resources/views/components/modal-review.blade.php
+++ resources/views/components/modal-review.blade.php
@@ -4,11 +4,12 @@
             <span class="icon-cross"></span>
         </button>
         <form action="{{route('send.review')}}" method="POST" class="form review-form">
-            @csrf
+            <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
             <input name="name" placeholder="Ваше имя*" required>
             <textarea name="comment" rows="6" minlength="8" maxlength="200" placeholder="Ваш отзыв" required></textarea>
             <input type="hidden" name="product_id" value="{{$product->id}}">
-            <button type="submit" class="review-form__button review-button-modal button button--color_red button--color-text_white">
+            <button type="submit"
+                    class="review-form__button review-button-modal button button--color_red button--color-text_white">
                 <span class="icon-arrow-right2"></span>
                 <span>Оставить отзыв</span>
             </button>
