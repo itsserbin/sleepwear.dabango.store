075b6f8b9a93a7329441beeec168a32519f69e3c
anymacstore <9911>
2021-05-04 23:14:20 +0300

    favicon

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -16,7 +16,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="{{asset('storage/favicon/favicon-32x32.png')}}">
 <link rel="icon" type="image/png" sizes="96x96" href="{{asset('storage/favicon/favicon-96x96.png')}}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{asset('storage/favicon/favicon-16x16.png')}}">
-<link rel="manifest" href="{{asset('mix-manifest.json')}}">
+<link rel="manifest" href="{{asset('storage/favicon/manifest.json')}}">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="{{asset('storage/favicon/ms-icon-144x144.png')}}">
 <meta name="theme-color" content="#ffffff">
