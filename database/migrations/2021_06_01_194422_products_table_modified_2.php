d2919e79fc7aaf31f1b41b72a7862da164cf81ab
itsserbin <serbin.official@gmail.com>
2021-06-02 19:18:34 +0300

    update

--- database/migrations/2021_06_01_194422_products_table_modified_2.php
+++ database/migrations/2021_06_01_194422_products_table_modified_2.php
@@ -15,7 +15,7 @@ class ProductsTableModified2 extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->string('viewed')->nullable()->change();
-            $table->foreign('provider_id')->constrained('providers')->onDelete('set null')->change();
+//            $table->foreign('provider_id')->constrained('providers')->onDelete('set null')->change();
         });
 
     }
