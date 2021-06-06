bed5fb8ed702caf864d9984387af269cb9ca103b
itsserbin <serbin.official@gmail.com>
2021-06-06 16:34:47 +0300

    update cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -182,7 +182,7 @@ class Kernel extends ConsoleKernel
                 }
 
                 $orders_days->investor_profit = ($SumDayMarginalityNow - (100 * $ReturnOrdersCountNow) * 0.35) - $SumCostsNow;
-                $orders_days->manager_salary = ($DoneOrdersCountNow + $CancelOrdersCountNow) * 15;
+                $orders_days->manager_salary = ($DoneOrdersCountNow + $ReturnOrdersCountNow) * 15;
                 $orders_days->save();
             } else {
                 foreach ($orders_old as $item) {
@@ -279,7 +279,7 @@ class Kernel extends ConsoleKernel
                     }
 
                     $item->investor_profit = ($SumDayMarginality - (100 * $ReturnOrdersCount) * 0.35) - $SumCosts;
-                    $item->manager_salary = ($DoneOrdersCount + $CancelOrdersCount) * 15;
+                    $item->manager_salary = ($DoneOrdersCount + $ReturnOrdersCount) * 15;
 
                     $item->update();
                 }
