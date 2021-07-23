ebe5539edaeded71823274836ee74874dc5d036a
itsserbin <serbin.official@gmail.com>
2021-07-24 00:13:15 +0300

    update fb products feed

--- resources/views/xml/fb-product-feed.blade.php
+++ resources/views/xml/fb-product-feed.blade.php
@@ -1,21 +1,27 @@
 @php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
-<listings>
-    <title>Dabango Products Feed</title>
+<rss version="2.0" xmlns:g="http://base.google.com/ns/1.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title>Dabango Products Feed</title>
+        <description>Product Feed for Facebook</description>
+        <link>{{asset('/')}}</link>
+        <atom:link href="{{route('fb.product.feed')}}" rel="self" type="application/rss+xml" />
+
     @if (!empty($products))
-        @foreach ($products as $item)
-            <listing>
-                <id>{{ $item->id }}</id>
-                <title>{{ $item->h1 }}</title>
-                <description>{{$item->description}}</description>
-                <availability>{{$item->status}}</availability>
-                <condition>new</condition>
-                @if($item->discount_price !== null)
-                    <price>{{$item->price}}</price>@endif
-                @if($item->discount_price)
-                    <sale_price>{{$item->discount_price}}</sale_price>@endif
-                <link>{{asset(route('product',$item->id))}}</link>
-                <image_link>{{asset($item->preview)}}</image_link>
-            </listing>
-        @endforeach
-    @endif
-</listings>
+            @foreach ($products as $item)
+                <item>
+                    <id>{{ $item->id }}</id>
+                    <title>{{ $item->h1 }}</title>
+                    <description>{{$item->description}}</description>
+                    <availability>{{$item->status}}</availability>
+                    <condition>new</condition>
+                    @if($item->discount_price !== null)
+                        <price>{{$item->price}}</price>@endif
+                    @if($item->discount_price)
+                        <sale_price>{{$item->discount_price}}</sale_price>@endif
+                    <link>{{asset(route('product',$item->id))}}</link>
+                    <image_link>{{asset($item->preview)}}</image_link>
+                </item>
+            @endforeach
+        @endif
+    </channel>
+</rss>
