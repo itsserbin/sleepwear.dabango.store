e36f7932593559f700f1e62224d73ba1a7b3625d
itsserbin <serbin.official@gmail.com>
2021-07-24 19:32:06 +0300

    update max height category preview

--- resources/js/components/category/CategoriesGrid.vue
+++ resources/js/components/category/CategoriesGrid.vue
@@ -4,7 +4,7 @@
         <div class="row" v-if="categories != 0 && !isLoading">
             <div class="col-12 col-xs-6 col-md-3" v-for="item in categories" :key="item.id">
                 <a v-bind:href="'/category/' + item.slug" class="card__product text-decoration-none my-3">
-                    <div class="card__image">
+                    <div class="card__image" style="max-height: 300px;">
                         <img :src="item.preview" alt="">
                     </div>
 
