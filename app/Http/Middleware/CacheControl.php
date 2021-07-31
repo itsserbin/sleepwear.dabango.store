9fdd974d1a57299c6c8eafd029a07c81f62f9aff
itsserbin <serbin.official@gmail.com>
2021-08-01 00:13:09 +0300

    update cache-control

--- app/Http/Middleware/CacheControl.php
+++ app/Http/Middleware/CacheControl.php
@@ -19,6 +19,7 @@ class CacheControl
     {
         $response = $next($request);
         $response->header('Cache-Control', 'private, max-age=0, no-cache');
+        $response->header('Expires', Carbon::now()->addYears(3));
         return $response;
     }
 }
