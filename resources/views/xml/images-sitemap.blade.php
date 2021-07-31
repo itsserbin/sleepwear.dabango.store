7c6dd959691efea020ebf4dcf5b30ab4ff7ec327
itsserbin <serbin.official@gmail.com>
2021-08-01 01:13:09 +0300

    update images_sitemap.xml

--- resources/views/xml/images-sitemap.blade.php
+++ resources/views/xml/images-sitemap.blade.php
@@ -1,5 +1,6 @@
 <?= '<' . '?' . 'xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
-<urlset xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     @foreach($products as $product)
         <url>
             <loc>{{route('product',$product->id)}}</loc>
