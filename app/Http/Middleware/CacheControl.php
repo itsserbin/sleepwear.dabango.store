587483cb133d0ad3e6079e9cb6203e611373a5de
itsserbin <serbin.official@gmail.com>
2021-07-31 16:09:19 +0300

    update cache-control

--- app/Http/Middleware/CacheControl.php
+++ app/Http/Middleware/CacheControl.php
@@ -11,8 +11,8 @@ class CacheControl
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
