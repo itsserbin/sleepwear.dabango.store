6cf53d62f5521b29f1b2b1b149147351c558aecd
itsserbin <serbin.official@gmail.com>
2021-11-01 20:20:27 +0200

    up

--- resources/js/components/category/ShowCategory.vue
+++ resources/js/components/category/ShowCategory.vue
@@ -4,32 +4,35 @@
             <div class="product-list__title">{{ category.title }}</div>
             <loader v-if="isLoading"></loader>
             <div class="row justify-content-center" v-if="!isLoading">
-                <a v-for="product in products"
-                   v-if="product.published === 1"
-                   v-bind:href="host + '/product/' + product.id"
-                   class="card__product text-decoration-none my-3">
+                <div v-for="product in products"
+                     v-if="product.published === 1"
+                     class="card__product text-decoration-none my-3"
+                >
+                    <a v-bind:href="host + '/product/' + product.id">
 
-                    <div class="card__image">
-                        <img :src="host + '/' + product.preview" alt="">
-                    </div>
-
-                    <div class="card__body">
-                        <h5 class="card__label">{{ product.h1 }}</h5>
-                        <div class="card__price">
-                            <div v-if="product.discount_price === null"
-                                 class="card__price-without-discount">{{ product.price }}
-                            </div>
-
-                            <div v-if="product.discount_price > null">
-                                <div class="card__old-price">{{ product.price }}</div>
-                                <div class="card__actual-price">{{ product.discount_price }}</div>
-                            </div>
-
-
+                        <div class="card__image">
+                            <img :src="host + '/' + product.preview" :alt="product.h1" style="object-fit: cover;">
                         </div>
-                        <span class="card__button">Подробнее</span>
-                    </div>
-                </a>
+
+                        <div class="card__body">
+                            <h5 class="card__label">{{ product.h1 }}</h5>
+                            <div class="card__price">
+                                <div v-if="product.discount_price === null"
+                                     class="card__price-without-discount">{{ product.price }}
+                                </div>
+
+                                <div v-if="product.discount_price > null">
+                                    <div class="card__old-price">{{ product.price }}</div>
+                                    <div class="card__actual-price">{{ product.discount_price }}</div>
+                                </div>
+
+
+                            </div>
+                            <span class="card__button">Подробнее</span>
+                        </div>
+                    </a>
+                </div>
+
             </div>
             <div class="row">
                 <div class="col">
